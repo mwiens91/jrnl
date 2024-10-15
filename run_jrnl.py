4a4bbc096839cb45f97b1f37fd8000af71b890a3
Matt Wiens <mwiens91@gmail.com>
2024-10-15 07:56:23 -0700

    Use Hatch to build instead of setuptools

--- run_jrnl.py
+++ run_jrnl.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 """Run the jrnl program."""
 
+import os
+import sys
+
+# Let me import jrnl
+# TODO: Is there a better way to do this without making src a package?
+os.chdir(os.path.join(os.getcwd(), "src"))
+sys.path.insert(0, "")
+
 from jrnl.main import main
 
 # Run it
